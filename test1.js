b9b8a90d56764849c115a019e8f9958a07cc69b3
jee007 <35541371+jee007@users.noreply.github.com>
2025-02-03 16:04:43 +0300

    Create test1.js
    all warehouse download

---
+++ test1.js
@@ -0,0 +1,33 @@
+(function() {
+    function loadScript(src, callback) {
+        var script = document.createElement('script');
+        script.src = src;
+        script.onload = callback;
+        document.body.appendChild(script);
+    }
+
+    function runScripts() {
+        loadScript('https://jee007.github.io/IB_ASN_PB_Details/psboxes.js', function() {
+            console.log('First loaded ' + new Date());
+            loadScript('https://jee007.github.io/IB_ASN_PB_Details/myasnscript.js', function() {
+                console.log('Second loaded ' + new Date());
+                loadScript('https://jee007.github.io/IB_ASN_PB_Details/wh_asn.js', function() {
+                    console.log('Third loaded ' + new Date());
+			loadScript('https://jee007.github.io/IB_ASN_PB_Details/psdetailsjed04.js', function() {
+            			console.log('First loaded ' + new Date());
+					loadScript('https://jee007.github.io/IB_ASN_PB_Details/myasnscriptjed04.js', function() {
+                				console.log('Second loaded ' + new Date());
+							loadScript('https://jee007.github.io/IB_ASN_PB_Details/wh_asnjed04.js', function() {
+            							console.log('First loaded ' + new Date());
+
+					});
+				});
+			});
+                });
+            });
+        });
+    }
+
+    runScripts();
+    setInterval(runScripts, 1800000); // Reload every 30 minutes
+})();
